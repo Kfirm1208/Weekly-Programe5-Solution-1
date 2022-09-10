549b35171a3d18f63c2bf3f838a21a2c5fee324b
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2022-09-10 15:43:42 +0700

    commits

--- Palindrom1.c
+++ Palindrom1.c
@@ -4,11 +4,15 @@
 int main(){
   char a[100],b[100];
 
-  printf("Enter the string to check if it is a palindrome: ");  
+  printf("Enter the number to check if it is a palindrome: ");  
   scanf("%[^\n]s",a);
+  // copy string
+
   strcpy(b,a);
+
   int j = strlen(b) - 1;
   
+  // check ที่ละตัวว่าเมื่อกันหรือปาว
   for(int i = 0 ;i < strlen(b);i++,j--){
 
     if(a[i] != a[j]){
